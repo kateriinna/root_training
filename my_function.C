4621a4da02172955e5fdf5aea478fece4b27d130
kateriinna <katerinasobatkevic@knu.ua>
2026-03-06 19:54:19 +0200

    Refactor function to streamline plotting process

--- my_function.C
+++ my_function.C
@@ -5,22 +5,16 @@ void my_function() {
     // 2. Створюємо функцію f(x) = sin(x)*x в діапазоні від 0 до 20
     TF1 *f1 = new TF1("f1", "sin(x)*x", 0, 20);
 
-    // 3. Рахуємо інтеграл і виводимо його на екран (як у тебе на скріншоті)
     double integral_val = f1->Integral(0, 10);
     printf("Integral(0, 10) = %f\n", integral_val);
 
-    // 4. Налаштовуємо заголовки графіка (англійською, щоб уникнути проблем зі шрифтами)
+
     f1->SetTitle("My Energy Spectrum Analysis");
     f1->GetXaxis()->SetTitle("Energy [GeV]");
     f1->GetYaxis()->SetTitle("Events / 0.1 GeV");
 
-    // 5. Налаштовуємо відступи та колір осі Х
     f1->GetYaxis()->SetTitleOffset(1.2);
     f1->GetXaxis()->SetTitleColor(kBlue);
 
-    // 6. Малюємо функцію
     f1->Draw();
-
-    // 7. Зберігаємо як картинку
-    c1->SaveAs("spectrum_analysis.png");
-}
+    c1->SaveAs("spectrum_analysis.png");}
